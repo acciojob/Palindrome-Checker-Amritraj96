dd05fb89d73e7a892a59ca077c794d210fed5fa8
Amritraj96 <amritraj.up.yadav@gmail.com>
2026-06-06 20:39:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
-// complete the given function
-
-function palindrome(str){
-
+function palindrome(str) {
+    const cleanStr = str.toLowerCase().replace(/[^a-z0-9]/g, '');
+    let left = 0;
+    let right = cleanStr.length - 1;
+    
+    while (left < right) {
+        if (cleanStr[left] !== cleanStr[right]) {
+            return false;
+        }
+        left++;
+        right--;
+    }
+    return true;
 }
-module.exports = palindrome
